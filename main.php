0e10b3a7d7bbb5a05ed7f44e0345efbab1c619e6
Liviu Coroncicu <liviucoronciuc@iesebre.com>
2015-10-13 21:01:31 +0200

    First Version

--- main.php
+++ main.php
@@ -35,9 +35,4 @@ class Controller
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
-
-if (isset($_GET['action']) && !empty($_GET['action'])) {
-    $controller->{$_GET['action']}();
-}
-
 echo $view->output();
